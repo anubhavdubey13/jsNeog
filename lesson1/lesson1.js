8eee45ef5f3790ab1abe9190c78587c49e96945e
Anubhav Dubey <anubhav.dubey.stats@gmail.com>
2021-12-08 21:11:11 +0530

    feat: Ex10-15 done
    Done with the lesson. Time for the Assignment.

--- lesson1/lesson1.js
+++ lesson1/lesson1.js
@@ -94,3 +94,91 @@ for (var i = 0; i < 5; i++){
 //=====================================================================
 
 // Homework: star pattern
+for (var i = 0; i < 5; i++){
+    var star_pat = '';
+    for (var j = 0; j < i+1; j++){
+        star_pat += '*';
+    }
+    console.log(star_pat);
+};
+
+// Bonus: Inverted pattern
+for (var i = 0; i < 5; i++){
+    var inv_star = '';
+    for (var j = 0; j < 5 - i; j++){
+        inv_star += '*';
+    }
+    console.log(inv_star);
+};
+
+//=====================================================================
+
+// Ex10: List grocery items to buy
+var groceryItems = ['aloo','chawal','daal','tel','namak'];
+// printing 1st, 3rd, 5th item
+console.log('First Item:', groceryItems[0], 'Third Item:', groceryItems[2], 'Fifth Item:', groceryItems[4]);
+
+//=====================================================================
+
+// Ex11: Print every item on the list
+for(var i = 0; i < groceryItems.length; i++){
+    console.log('Item Number', i+1, 'is', groceryItems[i]);
+};
+
+//=====================================================================
+
+// Ex12: Club info about a superhero together
+var superman = {
+    power: 500,
+    IQ: 300,
+    EQ: 800,
+    origin: 'Alien'
+};
+
+var batman = {
+    power: 100,
+    IQ: 700,
+    EQ: 400,
+    origin: 'Earth'
+};
+
+//=====================================================================
+
+// Ex13: Put a list of questions together
+questionOne = {
+    question: "Who is my favorite superhero?",
+    answer: "Bob"
+  }
+  
+  questionTwo = {
+    question: "Which is my favorite sad song?",
+    answer: "Deep Learning"
+  }
+
+//=====================================================================
+
+// Ex14: Club everything to make the game
+// Ex15: Print the final score to the user
+var quizScore = 0;
+var arrayQuestions = [questionOne, questionTwo];
+var highestScore = [2];
+
+function quiz(){
+    for (var i = 0; i < arrayQuestions.length; i++){
+        var userPrompt = readlineSync.question(arrayQuestions[i].question);
+        if (userPrompt.toLowerCase() === arrayQuestions[i].answer) {
+            score += 3;
+        } else {
+            score -= 1;
+        };
+    };
+    if (quizScore > highestScore) {
+        console.log(`Your final score is ${quizScore}. You have beaten the highest score. Send us the screenshot.`);
+    } else {
+        console.log(`Your final score is ${quizScore}. The highest score is ${highestScore[0]}`);   
+    }
+};
+
+quiz();
+
+//continue: https://neog.camp/guide/lesson-one
