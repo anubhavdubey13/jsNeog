5255511b1127b2b2ec93e3556da54e19f772d065
Anubhav Dubey <anubhav.dubey.stats@gmail.com>
2021-12-12 19:03:17 +0530

    feat + style: MCQ questions + colors using Chalk

--- lesson1/harryPotterQuiz.js
+++ lesson1/harryPotterQuiz.js
@@ -3,57 +3,63 @@
 // importing the module
 var readlineSync = require('readline-sync');
 
+// importing chalk. IMP: In replit, change the version to 4 instead of 5. 5 is not working right now
+var chalk = require('chalk');
+
+
 // questions and answers
 var q1 = {
-    question: 'Voldemort\'s full original name?',
+    question: 'Voldemort\'s full original name? ',
     answer: 'tom marvolo riddle'
 };
 
 var q2 = {
-    question: 'Arthur Weasely was married to whom? (First name only)',
+    question: 'Arthur Weasely was married to whom? (First name only) ',
     answer: 'molly'
 };
 
 var q3 = {
-    question: 'Most famous journalist in the magic world?',
+    question: 'Most famous journalist in the magic world? ',
     answer: 'rita skeeter'
 };
 
 var q4 = {
-    question: 'Mother of Albus Dumbledore? (First name only)',
+    question: 'Mother of Albus Dumbledore? (First name only) ',
     answer: 'kendra'
 };
 
 var q5 = {
-    question: 'How many brothers of Ginny?',
+    question: 'How many brothers of Ginny? ',
     answer: '6' // to avoid conflict later
 };
 
 // level 2 questions
 var q6 = {
-    question: 'Father of Draco Malfoy? First name only',
+    question: 'Father of Draco Malfoy? First name only ',
     answer: 'lucius'
 };
 
 var q7 = {
-    question: 'Who killed Nagini?',
+    question: 'Who killed Nagini? ',
     answer: 'neville'
 };
 
 var q8 = {
-    question: '"Always" is the famous uttering of which character? Full name.',
+    question: '"Always" is the famous uttering of which character? Full name. ',
     answer: 'severus snape'
 };
 
 // level 3 questions
 var q9 = {
-    question: 'The house of the cleverest folks:',
-    answer: 'ravenclaw'
+    question: 'The house of the cleverest folks: ',
+    options: ['Gryffindor','Hufflepuff', 'Ravenclaw', 'Slytherin'],
+    answer: 2 // since index starts from 0
 };
 
 var q10 = {
-    question: 'Animagus of Sirius Black:',
-    answer: 'dog'
+    question: 'Animagus of Sirius Black: ',
+    options: ['cat','wolf','rat','dog'],
+    answer: 3
 };
 
 // Quiz Version 2
@@ -75,49 +81,66 @@ var hScore = [11];
 function loopingOverArray(array, scr = 0) {
     var score = scr;
     for (var i = 0; i < array.length; i++){
-        var askUser = readlineSync.question(array[i].question);
+        var askUser = readlineSync.question(chalk.blue(array[i].question));
         console.log('\n');
         if (askUser.toLowerCase() === array[i].answer) {
             score += 3;
         } else {
             score -= 1;
         }
-        console.log(`Points Scored: ${score}\n`);
+        console.log(chalk.bold(`Points Scored: ${score}\n`));
+    }
+    return score;
+}
+
+// introducing a new function for different question type
+function multipleChoice(array, scr = 0) {
+    var score = scr;
+    for (var i = 0; i < array.length; i++){
+        var mcq = readlineSync.keyInSelect(array[i].options, array[i].question, {cancel:false});
+        console.log('\n');
+        if (mcq === array[i].answer) {
+            score += 3;
+        } else {
+            score -= 1;
+        };
+        console.log(chalk.bold(`Points Scored: ${score}\n`));
     }
     return score;
 }
 
 function harryPotter() {
     // asking username since I plan to use it at places
-    var userName = readlineSync.question('Enter your name');
-    console.log('Let\'s see what you can do', userName);
+    var userName = readlineSync.question(chalk.bold('Enter your name: '));
+    console.log('\n');
+    console.log(chalk.red.bold('Rules:\n1. Cheating is allowed\n2. Focus on spellings\n3. Correct: 3 points, Incorrect: -1 points\n4. Press Enter after answering'));
+    console.log('\n');
+    console.log(chalk.bold('Let\'s see what you can do', chalk.italic(userName)));
+    console.log('\n');
     // the main loop for question
-    var scoreLevelOne = Math.max(loopingOverArray(levelOne), 0);
+    //var scoreLevelOne = loopingOverArray(levelOne);
+    var cumulativeScore = loopingOverArray(levelOne);
 
-    if (scoreLevelOne === 15) {
-        console.log('\nWohooo! You made it to the Second Level!!\n');
-        var scoreLevelTwo = Math.max(loopingOverArray(levelTwo), 0);
-        if (scoreLevelTwo === 9) {
-            console.log('\nDamn good potterhead! Keep Going! Level three is waiting for you!!');
-            var scoreLevelThree = Math.max(loopingOverArray(levelThree), 0);
+    if (cumulativeScore === 15) {
+        console.log(chalk.green.bold('\nWohooo! You made it to the Second Level!!\n'));
+        var cumulativeScore = loopingOverArray(levelTwo,cumulativeScore);
+        if (cumulativeScore === 9) {
+            console.log(chalk.green.bold('\nDamn good potterhead! Keep Going! Level three is waiting for you!!'));
+            var cumulativeScore = multipleChoice(levelThree,cumulativeScore);
         } 
     }
 
-    var fScore = scoreLevelOne + scoreLevelTwo + scoreLevelThree;
+    var fScore = cumulativeScore;
 
     // logging score basis user performance wrt highest score
     if (fScore > hScore[0]) {
-        console.log(`${userName}!!!! You have beaten the highest score. Your final score is ${fScore}. Send me the screenshot!`);
+        console.log(chalk.bgGreen.bold(`${userName}!!!! You have beaten the highest score. Your final score is ${fScore}. Send me the screenshot!`));
     } else {
-        console.log(`${userName}, you have scored ${fScore} points.`);
+        console.log(chalk.bgRed.bold(`${userName}, you have scored ${fScore} points.`));
     };
 };
 
 harryPotter();
 
-// Add-ons
-/*
-2. Readlinesync extra functions
-3. Use chalk
-https://neog.camp/guide/lesson-one
-*/
+
+// https://neog.camp/guide/lesson-one
