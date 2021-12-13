2ff734920fe61e0987f3c364ca7c22f20459b716
Anubhav Dubey <anubhav.dubey.stats@gmail.com>
2021-12-13 14:14:39 +0530

    feat: check input format; fix: leap year check; style: used chalk

--- lesson1/leapYear.js
+++ lesson1/leapYear.js
@@ -3,26 +3,46 @@
 // importing readline sync
 var readlineSync = require('readline-sync');
 
-// leap year calculator function
-function leapYearCheck(year){
-    if (year%100 === 0) {
-        if (year%400 === 0) {
-            return true;
-        } else {
-            return false;
-        }
-    } else if (year%4 === 0){
+// importing chalk. IMP: In replit, change the version to 4 instead of 5. 5 is not working right now
+var chalk = require('chalk');
+
+// valid format check
+function isValidFormat(year){
+    var validFormat = /^\d{2}-\d{2}-\d{4}$/;
+    if (year.match(validFormat)){
         return true;
     } else {
         return false;
     }
 };
 
+// leap year calculator function
+function leapYearCheck(year){
+    var iVF = isValidFormat(year);
+    if (iVF) {
+        if (year%100 === 0) {
+            if (year%400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (year%4 === 0){
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        console.log(chalk.red.bold.bgWhite("Invalid Input Format!! Kindly try again."));
+        break;
+    }
+    
+};
+
 // getting birthday in a format & extracting the year
 // replit is a beauty
 function yourBirthday(){
-    console.log('Input your birthday in "dd-mm-yyyy" format.');
-    console.log('If you were born on 14 Jan 2001, enter 14-01-2001');
+    console.log(chalk.bold.blue('Input your birthday in "dd-mm-yyyy" format.'));
+    console.log(chalk.bold.blue('If you were born on 14 Jan 2001, enter 14-01-2001'));
     var userBirthday = readlineSync.question();
     var yearOfBirth = userBirthday.substr(-4);
     return yearOfBirth; 
@@ -30,14 +50,14 @@ function yourBirthday(){
 
 // final app
 function leap(){
-    var userName = readlineSync.question('Naam kya hai tumhara? What\'s your name?');
-    console.log('Welcome,', userName, '!');
+    var userName = readlineSync.question(chalk.bold('Naam kya hai tumhara? What\'s your name?'));
+    console.log(chalk.bold('Welcome,', userName, '!'));
     var year = yourBirthday();
     var leapYear = leapYearCheck(year);
     if (leapYear) {
-        console.log(`Woah, ${userName}! You were born in a leap year. Share it on Social Media!`);
+        console.log(chalk.bold(`Woah, ${userName}! You were born in a leap year. Share it on Social Media!`));
     } else {
-        console.log(`${userName}, 75% of extra-ordinary people are born in non-leap year! You are one of them. Share on your socials!`);
+        console.log(chalk.bold(`${userName}, 75% of extra-ordinary people are born in non-leap year! You are one of them. Share on your socials!`));
     };
 };
 
