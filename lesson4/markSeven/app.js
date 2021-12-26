2d8ca8ee4bb3eff9c91aa98e211cb0b87fc9641f
Anubhav Dubey <anubhav.dubey.stats@gmail.com>
2021-12-26 12:02:23 +0530

    feat: Translator using external API

---
+++ lesson4/markSeven/app.js
@@ -0,0 +1,31 @@
+var inputText = document.querySelector('.inputText');
+var button = document.querySelector('#btn-translate');
+var output = document.querySelector('#output-translated');
+
+//var tanayURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+var shakesURL = 'https://api.funtranslations.com/translate/shakespeare.json';
+
+function errorHandler(error){
+    console.log('Something wrong!', error);
+    alert('Server error! Please try again after some time.');
+};
+
+function translator(url, text){
+    var full = url + '?text=' + text;
+    var translation = fetch(full)
+                        .then(response => response.json())
+                        .then(json => {
+                            var translated = json.contents.translated;
+                            output.innerText = translated;
+                        })
+                        .catch(errorHandler);
+    return translation;
+};
+
+function callback(){
+    //console.log('Button Clicked!')
+    translator(shakesURL, inputText.value);
+};
+
+button.onclick = callback;
+
