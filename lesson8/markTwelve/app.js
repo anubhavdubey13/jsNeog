6bebed178c29dea9e7c4a8cfbb3c57a99ccd129c
Anubhav Dubey <anubhav.dubey.stats@gmail.com>
2022-01-27 13:07:49 +0530

    feat: on click display and hiding of the games

--- lesson8/markTwelve/app.js
+++ lesson8/markTwelve/app.js
@@ -1,4 +1,46 @@
 // display functionality
+var angles = document.querySelector('.angles');
+angles.style.display='none';
+var quiz = document.querySelector('.quiz');
+quiz.style.display='none';
+var hypotenuse = document.querySelector('.hypotenuse');
+hypotenuse.style.display='none';
+var area = document.querySelector('.area');
+area.style.display='none';
+
+var funAngles = document.querySelector('#funAngles');
+var funQuiz = document.querySelector('#funQuiz');
+var funHypotenuse = document.querySelector('#funHypotenuse');
+var funArea = document.querySelector('#funArea');
+
+funAngles.addEventListener('click', ()=>{
+    angles.style.display="block";
+    quiz.style.display='none';
+    hypotenuse.style.display='none';
+    area.style.display='none';
+});
+
+funQuiz.addEventListener('click', ()=>{
+    angles.style.display="none";
+    quiz.style.display='block';
+    hypotenuse.style.display='none';
+    area.style.display='none';
+});
+
+funHypotenuse.addEventListener('click', ()=>{
+    angles.style.display="none";
+    quiz.style.display='none';
+    hypotenuse.style.display='block';
+    area.style.display='none';
+});
+
+funArea.addEventListener('click', ()=>{
+    angles.style.display="none";
+    quiz.style.display='none';
+    hypotenuse.style.display='none';
+    area.style.display='block';
+});
+
 // Mad Angles
 var angleOne = document.querySelector('#angleOne');
 var angleTwo = document.querySelector('#angleTwo');
@@ -61,7 +103,7 @@ var sideTwo = document.getElementById('sideTwo');
 var hypotenuseSubmit = document.getElementById('hypotenuseSubmit');
 var hypotenuseOutput = document.getElementById('hypotenuseOutput');
 
-function hypotenuse(){
+function hypotenuseFunction(){
     var s1 = Number(sideOne.value);
     var s2 = Number(sideTwo.value);
     if (s1 && s2) {
@@ -74,7 +116,7 @@ function hypotenuse(){
 
 function callbackHypo(){
     //console.log('Clicked Hypotenuse');
-    hypotenuseOutput.innerHTML = hypotenuse();
+    hypotenuseOutput.innerHTML = hypotenuseFunction();
 }
 
 hypotenuseSubmit.addEventListener('click', callbackHypo);
@@ -90,7 +132,8 @@ function areaTriangle(){
     var h = Number(height.value);
     if (b && h){
         var ar = 0.5*b*h;
-        return ar;
+        var finalSentence = `${ar} sq. cm`;
+        return finalSentence;
     } else {
         return 'Enter both fields';
     }
